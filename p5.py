ff7d0d33220574b61da0e4df06536375cb5ad4c6
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-28 17:21:30 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -6,14 +6,26 @@ import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 import RPi.GPIO as GPIO
-btn=16
+
+#import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+def button_callback(channel):
+    print("Button was pushed!")
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
 
-while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
+
+#btn=16
+#GPIO.setwarnings(False) # Ignore warning for now
+##GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+#GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+
+#while True: # Run forever
+    #if GPIO.input(10) == GPIO.HIGH:
+        #print("Button was pushed!")
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
