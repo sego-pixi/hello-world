471892afe285cf2f60009819df86dcc0d48cfbe0
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-28 21:34:14 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -54,7 +54,7 @@ def printit():
         x=chan.voltage-0.5
         temperature=x/0.01
         #print("Here",temperature)
-        sec=round(time.clock())
+        sec=round(time.process_time())
         print("{:2d}s".format(sec),end="")
         print("{:-10d} {:-17.1f} C".format(chan1.value,temperature))
         
