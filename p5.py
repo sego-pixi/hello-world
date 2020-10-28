f3d2888ce897125a8cccc896e53f2ea58a8da116
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-28 17:16:48 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -9,7 +9,7 @@ import RPi.GPIO as GPIO
 btn=16
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
