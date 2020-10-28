fdcfb02bcc6ee73dd677a5f1ded0b738ff47eeaa
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-28 17:13:53 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -8,7 +8,7 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 import RPi.GPIO as GPIO
 btn=16
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setmode(16) # Use physical pin numbering
 GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 
 while True: # Run forever
