b304f6c69e2d6d1269e56448e74bfcea7c6c1cb3
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-29 15:10:23 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -10,9 +10,6 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-count=0
-ButtonState= True
-
 while True:
     if(GPIO.input(16)):
         print("BUtton pressed")
