761b7286b32926f1b8414d84ce168406fcd80feb
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-29 11:35:39 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -10,13 +10,13 @@ import RPi.GPIO as GPIO
 
 
 
- btn=10
- GPIO.setwarnings(False) # Ignore warning for now
- GPIO.setmode(GPIO.BCM) # Use physical pin numbering
- GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
- while True: # Run forever
-     if GPIO.input(10) == GPIO.HIGH:
-         print("Button was pushed!")
+btn=10
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")
 
 
 # create the spi bus
