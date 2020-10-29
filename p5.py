265e14416a65d3e0821eda62570825664090ea95
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-29 22:09:15 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -54,6 +54,7 @@ def get_press():
                 #count=0
                 #s=interval[count]
                 count+=1
+                time.sleep(5)
                 #if count==3:                       
                         #count=0
                 print(count)
