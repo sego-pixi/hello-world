4655efc43079cd978952c811b1ad5e91bce0408a
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-29 11:17:49 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -40,7 +40,8 @@ print("ADC Voltage: " + str(chan.voltage) + "V")
 x=chan.voltage-0.5
 temperature=x/0.01
 print("Runtime    Temp Reading    Temp") #spaces of 4 between words
-print("0s   ",chan1.value,temperature)
+#print("0s     ",chan1.value,temperature)
+print("0s    {:-10d} {:-17.1f} C".format(chan1.value,temperature))
 start=time.time()
 def printit():
       
