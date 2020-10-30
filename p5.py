858d13b9abfed3a95cd0a159eaab1102ac7a6307
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-10-30 09:37:43 +0200

    Update p5.py

--- p5.py
+++ p5.py
@@ -79,7 +79,7 @@ def printit():
             
        start_helper_thread()     
        #print(get_press())
-       #thread = threading.Timer(s, printit).start()  
+       thread = threading.Timer(s, printit).start()  
        x=chan.voltage-0.5
        temperature=x/0.01
        time.sleep(s)
