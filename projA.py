a1781a33d7364c88a6a77c59d1a96817683dcabf
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-02 12:44:30 +0200

    Create projA.py

---
+++ projA.py
@@ -0,0 +1,9 @@
+import threading
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import RPi.GPIO as GPIO
+from _thread import start_new_thread
