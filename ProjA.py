93bd98a90950850e8e9ca7d346b7e91d94945074
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-08 14:06:27 +0200

    Update ProjA.py

--- ProjA.py
+++ ProjA.py
@@ -28,10 +28,10 @@ mcp = MCP.MCP3008(spi, cs)
 
 # create an analog input channel on pin 1
 chan = AnalogIn(mcp, MCP.P1)
-while True:    
-  if(GPIO.input(16)):       
-    start=time.time()
-#start=time.time()
+# while True:    
+#   if(GPIO.input(16)):       
+#     start=time.time()
+start=time.time()
 #start=0
 print("Press button to start monitoring the sensor")
 def clear(): 
