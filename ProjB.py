52872b4aed3b7fd9ac0c58ed88b6310705dd2a42
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-08 12:58:01 +0200

    Create ProjB.py

---
+++ ProjB.py
@@ -0,0 +1,30 @@
+import threading
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import RPi.GPIO as GPIO
+from _thread import start_new_thread
+from datetime import datetime
+from os import system, name
+import sys
+import subprocess, os
+import signal
+
+#this is for the start/stop button (Changed pin to 16)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 1
+chan = AnalogIn(mcp, MCP.P1)
