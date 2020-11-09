6fbfd62c470eb6d9b85f4bf6eff828d320efdf9d
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-09 23:02:59 +0200

    Update blynk.py

--- blynk.py
+++ blynk.py
@@ -1,1 +1,28 @@
 import BlynkLib
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3,GPIO.OUT)
+GPIO.setwarnings(False)
+
+BLYNK_AUTH='7t6Q72qVqWmrxPWydL0F18TEWejod_7P'
+blynk=BlynkLib.Blynk(BLYNK_AUTH)
+
+@blynk.VIRTUAL_WRITE(1)
+def write_to_pin(pin,value):
+  GPIO.output(3,1)
+  print('Current V1 value: {}'.format(value))
+  
+while True:
+  blynk.run()
+    
+  
+  #sensor_data=20
+  #blynk.virtual_write(1,sensor_data)
+  #blynk.virtual_write(pin,25)
+# @blynk.VIRTUAL_READ(2)
+# def my_user_task():
+#     # this widget will show some time in seconds..
+#     blynk.virtual_write(2, 45)
+#     blynk.virtual_write(3, 50)
+# blynk.set_user_task(my_user_task,3000)
