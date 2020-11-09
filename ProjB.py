fd29d0889dd97e1ac7290d9013bfaf4bd71461f6
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-09 18:11:07 +0200

    Update ProjB.py

--- ProjB.py
+++ ProjB.py
@@ -118,7 +118,7 @@ def pressBtn():
             print("  00:00:00",end="")
             print("{:-8.1f} C".format(temperature))
             printit()
-            count=1;
+            count=1
           elif count==1:
             print("logging has stopped")
             time.sleep(4)
