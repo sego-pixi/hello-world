e46fb9ab16afc7ef7dd226577d8071402d3d2362
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-10 19:51:04 +0200

    Update ProjB.py

--- ProjB.py
+++ ProjB.py
@@ -16,7 +16,11 @@ import BlynkLib
 
 BLYNK_AUTH='MuR0By29Wg-I38uvkZoSLe3vfP6iyyR7'
 blynk=BlynkLib.Blynk(BLYNK_AUTH)
-
+#this is for the start/stop button (Changed pin to 16)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#this is for the interval button
+GPIO.setup(15,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -29,6 +33,9 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 1
 chan = AnalogIn(mcp, MCP.P0)
 
+
+
+start=time.time()
 t=5#initially
 def clear(): 
   
@@ -180,57 +187,54 @@ def main():
   #BLYNK_AUTH='7t6Q72qVqWmrxPWydL0F18TEWejod_7P'
  
 
-  #this is for the start/stop button (Changed pin to 16)
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(16,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-  #this is for the interval button
-  GPIO.setup(15,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
   # create the spi bus
   
   
-  start=time.time()
   #print("Press button to start monitoring the sensor")
-  GPIO.add_event_detect(16,GPIO.RISING,callback=button_callback)
-  message = input("Press button to start monitoring the sensor\n")
+  #GPIO.add_event_detect(16,GPIO.RISING,callback=button_callback)
+  #message = input("Press button to start monitoring the sensor\n")
   
     
 #     global btn_already_pressed;
 #     global count
-#     while True:
-#         #blynk.run()
-#         if(GPIO.input(16)):
-#             print("here")
-#           #if count==0:
+  while True:
+    
+    blynk.run()
+    if(GPIO.input(16)):
+        
+        if count==0:
             
             
-#             print("logging started")
-#             #sensor_data=20
-#             #blynk.virtual_write(1,sensor_data)
-#             #write_to_pin(4)
-#             blynk.run()
-#             blynk.virtual_write(1,"clr")
-#             now= datetime.now()
-#             current_time = now.strftime("%H:%M:%S")
-#             x=chan.voltage-0.5
-#             temperature=x/0.1
-#             print("Time      Sys Timer  Temp") #spaces of 4 between words
-#             blynk.virtual_write(1,"Time       Sys Timer      Temp\n")
-#             print(current_time,end="")
-#             blynk.virtual_write(1,current_time)
-#             print("  00:00:00",end="")
-#             blynk.virtual_write(1,"   00:00:00       ")
-#             print("{:-8.1f} C".format(temperature))
-#             newtemp=round(temperature,1)
-#             blynk.virtual_write(1,newtemp,"\n")
-# #            printit()
-#             if count ==1:
-#                 print("1..logging has stopped")
-#                 time.sleep(4)
-#                 clear()              
-#                 print("Logging has stopped you can now exit the program")
-#                 time.sleep(15)
-#                 break
+            print("logging started")
+            #sensor_data=20
+            #blynk.virtual_write(1,sensor_data)
+            #write_to_pin(4)
+            blynk.run()
+            blynk.virtual_write(1,"clr")
+            now= datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            x=chan.voltage-0.5
+            temperature=x/0.1
+            print("Time      Sys Timer  Temp") #spaces of 4 between words
+            blynk.virtual_write(1,"Time       Sys Timer      Temp\n")
+            print(current_time,end="")
+            blynk.virtual_write(1,current_time)
+            print("  00:00:00",end="")
+            blynk.virtual_write(1,"   00:00:00       ")
+            print("{:-8.1f} C".format(temperature))
+            newtemp=round(temperature,1)
+            blynk.virtual_write(1,newtemp,"\n")
+            printit()
+            count=1
+        elif count ==1:
+                print("1..logging has stopped")
+                time.sleep(4)
+                clear()              
+                print("Logging has stopped you can now exit the program")
+                time.sleep(15)
+                break
 #               start_helper_thread()
               
 #             count=1
