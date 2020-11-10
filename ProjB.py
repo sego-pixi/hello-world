01d7624c387dd8ec3a58efb02c61b53d6ae89557
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-10 19:09:42 +0200

    Update ProjB.py

--- ProjB.py
+++ ProjB.py
@@ -103,7 +103,27 @@ def printit():
         
         
 def button_callback(channel):
-    print("Button was pushed!")          
+    print(channel)
+    print("Button was pushed!")
+    print("logging started")
+    sensor_data=20
+    blynk.virtual_write(1,sensor_data)
+    write_to_pin(4)
+    blynk.run()
+    blynk.virtual_write(1,"clr")
+    now= datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    x=chan.voltage-0.5
+    temperature=x/0.1
+    print("Time      Sys Timer  Temp") #spaces of 4 between words
+    blynk.virtual_write(1,"Time       Sys Timer      Temp\n")
+    print(current_time,end="")
+    blynk.virtual_write(1,current_time)
+    print("  00:00:00",end="")
+    blynk.virtual_write(1,"   00:00:00       ")
+    print("{:-8.1f} C".format(temperature))
+    newtemp=round(temperature,1)
+    blynk.virtual_write(1,newtemp,"\n")
                 
 def main():
   #BLYNK_AUTH='7t6Q72qVqWmrxPWydL0F18TEWejod_7P'
