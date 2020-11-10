4f2380e35281a9f7fd0f5aa68c7d21dae26a0893
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-10 19:58:33 +0200

    Update ProjB.py

--- ProjB.py
+++ ProjB.py
@@ -14,6 +14,7 @@ import subprocess, os
 import signal
 import BlynkLib
 
+#BLYNK_AUTH='7t6Q72qVqWmrxPWydL0F18TEWejod_7P'
 BLYNK_AUTH='MuR0By29Wg-I38uvkZoSLe3vfP6iyyR7'
 blynk=BlynkLib.Blynk(BLYNK_AUTH)
 #this is for the start/stop button (Changed pin to 16)
@@ -36,6 +37,7 @@ chan = AnalogIn(mcp, MCP.P0)
 
 
 start=time.time()
+print("Press button to start monitoring the sensor")
 t=5#initially
 def clear(): 
   
@@ -108,96 +110,9 @@ def printit():
               newtemp=round(temperature,1)
               blynk.virtual_write(1,"       ")
               blynk.virtual_write(1,newtemp,"\n")
-              
-              
-#               if(GPIO.input(16)):
-#                count2=1
-#                print("here")
-              #count2=1
-            
-            
-#         if count2==1:
-#             print("1..logging has stopped")
-#             time.sleep(4)
-#             clear()              
-#             print("Logging has stopped you can now exit the program")
-#             time.sleep(15)
-        
-        
-def button_callback(channel):
-  
-    global start
-    if start:      
-      print("Button was pushed!")
-      print("logging started")
-      #sensor_data=20
-      
-      blynk.run()
-      blynk.virtual_write(1,"clr")
-      now= datetime.now()
-      current_time = now.strftime("%H:%M:%S")
-      x=chan.voltage-0.5
-      temperature=x/0.1
-      print("Time      Sys Timer  Temp") #spaces of 4 between words
-      blynk.virtual_write(1,"Time       Sys Timer      Temp\n")
-      print(current_time,end="")
-      blynk.virtual_write(1,current_time)
-      print("  00:00:00",end="")
-      blynk.virtual_write(1,"   00:00:00       ")
-      print("{:-8.1f} C".format(temperature))
-      newtemp=round(temperature,1)
-      blynk.virtual_write(1,newtemp,"\n")
-      start_helper_thread()
-      
-      start=False
-      while not start:
-        message = input("")
-        x=chan.voltage-0.5
-        temperature=x/0.1
-        #thread = threading.Timer(5, printit).start()
-        time.sleep(t)
-        now= datetime.now()        
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time,end="")   
-        blynk.virtual_write(1,current_time)
-                        #time.sleep(5)
-        end=time.time()
-        duration=end-start
-        secs=round(duration)
-                #sensor_data=20             
-                #blynk.virtual_write(1,sensor_data)
-        m, s = divmod(secs, 60)
-        h, m = divmod(m, 60)
-        print('  {:02d}:{:02d}:{:02d}'.format(h, m, s),end="") 
-        blynk.virtual_write(1,'   {:02d}:{:02d}:{:02d}'.format(h, m, s))
-        print("{:-8.1f} C".format(temperature))
-        newtemp=round(temperature,1)
-        blynk.virtual_write(1,"       ")
-        blynk.virtual_write(1,newtemp,"\n")
-    else:
-      
-      print("1..logging has stopped")
-      time.sleep(4)
-      clear()              
-      print("Logging has stopped you can now exit the program")
-      time.sleep(15)
-      
-                
+                             
 def main():
-  #BLYNK_AUTH='7t6Q72qVqWmrxPWydL0F18TEWejod_7P'
- 
-
-
-
-  # create the spi bus
   
-  
-  #print("Press button to start monitoring the sensor")
-  #GPIO.add_event_detect(16,GPIO.RISING,callback=button_callback)
-  #message = input("Press button to start monitoring the sensor\n")
-  
-    
-#     global btn_already_pressed;
   global count
   while True:
     
@@ -208,9 +123,6 @@ def main():
             
             
             print("logging started")
-            #sensor_data=20
-            #blynk.virtual_write(1,sensor_data)
-            #write_to_pin(4)
             blynk.run()
             blynk.virtual_write(1,"clr")
             now= datetime.now()
@@ -235,43 +147,6 @@ def main():
                 print("Logging has stopped you can now exit the program")
                 time.sleep(15)
                 break
-#               start_helper_thread()
-              
-#             count=1
-               
-# #             print("now 1")
-            
-            
-#           elif count==1:
-              
-#               x=chan.voltage-0.5
-#               temperature=x/0.1
-#               #thread = threading.Timer(5, printit).start()
-#               time.sleep(t)
-#               now= datetime.now()        
-#               current_time = now.strftime("%H:%M:%S")
-#               print(current_time,end="")   
-#               blynk.virtual_write(1,current_time)
-#                       #time.sleep(5)
-#               end=time.time()
-#               duration=end-start
-#               secs=round(duration)
-#               #sensor_data=20             
-#               #blynk.virtual_write(1,sensor_data)
-#               m, s = divmod(secs, 60)
-#               h, m = divmod(m, 60)
-#               print('  {:02d}:{:02d}:{:02d}'.format(h, m, s),end="") 
-#               blynk.virtual_write(1,'   {:02d}:{:02d}:{:02d}'.format(h, m, s))
-#               print("{:-8.1f} C".format(temperature))
-#               newtemp=round(temperature,1)
-#               blynk.virtual_write(1,"       ")
-#               blynk.virtual_write(1,newtemp,"\n")
-    
-#             print("1..logging has stopped")
-#             time.sleep(4)
-#             clear()              
-#             print("Logging has stopped you can now exit the program")
-#             time.sleep(15)
 
             
 main()
