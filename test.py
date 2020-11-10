dd2efb1017f2e42cea1d47adedef7eb14345ed2f
Lesego Bikitshi <52962330+sego-pixi@users.noreply.github.com>
2020-11-10 18:48:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 
 GPIO.add_event_detect(16,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 
-message = input("Press enter to quit\n\n") # Run until someone presses enter
+#message = input("Press enter to quit\n\n") # Run until someone presses enter
 
 #GPIO.cleanup() # Clean up
